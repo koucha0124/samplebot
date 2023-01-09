0888efcce465699e8e38703cd3b20636eabf4bf9
こうちゃ <84267040+koucha0124@users.noreply.github.com>
2023-01-09 23:50:10 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 bot.remove_command("help")
 
 @bot.event
